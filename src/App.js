9f386a35c9c7d26092104d532d238181a51137d2
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-05-29 23:17:59 +0100

    props children

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 
 class App extends React.Component {
   constructor() {
@@ -34,7 +35,9 @@ class App extends React.Component {
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <SearchBox onSearchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
       </div>
     );
   }
